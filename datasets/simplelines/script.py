c8923d4773ae1ecaa6b6f02870e616156bd965f5
chellick <matsupus@gmail.com>
2023-10-05 18:52:38 +0300

    upd.

--- datasets/simplelines/script.py
+++ datasets/simplelines/script.py
@@ -9,7 +9,7 @@ def create_dataset(size):
             draw = ImageDraw.Draw(im)
             draw.line(((0, random.randint(1, im.size[0])), (random.randint(1, im.size[1]), 0)) + im.size, width=10, fill=128)    
             hr = im
-            lr = im.resize([im.size[0]// 10, im.size[1]// 10])
+            lr = im.resize([im.size[0]// , im.size[1]// 10])
             hr.save(f"datasets/simplelines/hr/{i}.png")
             lr.save(f"datasets/simplelines/lr/{i}.png")
 
