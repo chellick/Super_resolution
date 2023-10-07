6aec8c8d0ea1dca0b33666f3276dd99481f21b49
chellick <matsupus@gmail.com>
2023-10-07 13:44:23 +0300

    upd.

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
 import tensorflow as tf
-print(tf.config.list_physical_devices('GPU'))
+
+gpus = tf.config.list_physical_devices('GPU')
+if gpus:
+  # Currently, we allow TensorFlow to use only one GPU
+  try:
+    tf.config.experimental.set_visible_devices(gpus[0], 'GPU')
+    print("GPU is available", gpus)
+  except RuntimeError as e:
+    # Visible devices must be set before GPUs have been initialized
+    print(e)
+else:
+  print("No GPU available", tf.__version__)
