ef752a08a2e647df004f38dc4751c94a74f12c02
chellick <matsupus@gmail.com>
2023-11-19 11:14:52 +0300

    pc upd.

--- datasets/simplelines/script.py
+++ datasets/simplelines/script.py
@@ -3,14 +3,14 @@ from PIL import Image, ImageDraw
 import random
 
 
+
 def create_dataset(size):
     for i in range(size):
         with Image.open("datasets/simplelines/main.png") as im:
-            draw = ImageDraw.Draw(im)
-            draw.line(((0, random.randint(1, im.size[0])), (random.randint(1, im.size[1]), 0)) + im.size, width=10, fill=128)    
+            draw = ImageDraw.Draw(im.resize(100, 100))
+            draw.line(((0, random.randint(1, 100)), (random.randint(1, 100), 0)) + im.size, width=10, fill=128)    
             hr = im
-            lr = im.resize([im.size[0]// , im.size[1]// 10])
             hr.save(f"datasets/simplelines/hr/{i}.png")
-            lr.save(f"datasets/simplelines/lr/{i}.png")
+
 
 create_dataset(1000)
