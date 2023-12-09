6dab1d78e67522fff6eab68b10e2f517c4a2b0d5
chellick <matsupus@gmail.com>
2023-12-10 00:05:49 +0300

    image byteIO upd.

--- tgbot/main.py
+++ tgbot/main.py
@@ -2,6 +2,7 @@ import asyncio
 import logging
 import sys
 import glob
+from PIL import Image
 from aiogram import F
 from aiogram import Bot, Dispatcher, Router, types
 from aiogram.enums import ParseMode
@@ -9,6 +10,7 @@ from aiogram.filters import CommandStart, Command
 from aiogram.types import Message
 from aiogram.utils.markdown import hbold
 from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import State, StatesGroup
 
 
 with open("tgbot/TOKEN.txt") as f:
@@ -16,8 +18,6 @@ with open("tgbot/TOKEN.txt") as f:
 
 dp = Dispatcher()
 sp = glob.glob("tgbot/temp")
-
-bot = Bot(TOKEN, parse_mode=ParseMode.HTML)
 
 
 @dp.message(CommandStart())
@@ -28,14 +28,19 @@ async def command_start_handler(message: Message) -> None:
 @dp.message(F.photo)
 async def download_photo(message: Message):
     print(message.photo[-1])
-    await bot.download(
-            message.photo[-1],
-            destination=f"C:/python/GitHub/Super_resolution/tgbot/temp/x_train.png"
-        )
+
+    photo = await message.bot.download(
+        message.photo[-1])
     
+    photo.seek(0)
+    init_photo = Image.open(photo)
+    init_photo.show()
+    print(init_photo)
+
 
 
 async def main() -> None:
+    
     bot = Bot(TOKEN, parse_mode=ParseMode.HTML)
     await dp.start_polling(bot)
     
